1ea655657314ed1d24cdee7b5c29220e28291b14
Thanh Diep Van <thanh.diep.intern@ntq-solution.com.vn>
2018-12-08 10:41:41 +0700

    inintal commit

---
+++ src/main/java/seleniumautomationtest/SeleniumAutomationTest/App.java
@@ -0,0 +1,13 @@
+package seleniumautomationtest.SeleniumAutomationTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
