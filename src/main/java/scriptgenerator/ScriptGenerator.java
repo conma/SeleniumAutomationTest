ab7b835b34888867556e81ef098b13f2a62895f6
Thanh Diep Van <thanh.diep@ntq-solution.com.vn>
2018-12-19 08:53:49 +0700

    add ScriptGener..

--- src/main/java/scriptgenerator/ScriptGenerator.java
+++ src/main/java/scriptgenerator/ScriptGenerator.java
@@ -1,61 +1,86 @@
 package scriptgenerator;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-public class ScriptGenerator {
-    public static void main(String[] args) {
-        try {
-            scriptGenerator("testcases/Testcases.xls");
-        } catch (EncryptedDocumentException | IOException e) {
+public class ScriptGenerator
+{
+    private static final String TAB = "\t";
+
+    private static final String NEW_LINE = "\n";
+
+    public static void main( String[] args )
+    {
+        try
+        {
+            scriptGenerator( "testcases/Testcases.xls", "scripts/Tescases.txt" );
+        }
+        catch ( EncryptedDocumentException | IOException e )
+        {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
-    public static void scriptGenerator(String filePath) throws EncryptedDocumentException, IOException {
-        InputStream testcasesInputStream = new FileInputStream(filePath);
-        Workbook wb = WorkbookFactory.create(testcasesInputStream);
+    public static void scriptGenerator( String testcaseFilePath, String scriptFilePath ) throws EncryptedDocumentException, IOException
+    {
+        InputStream testcasesInputStream = new FileInputStream( testcaseFilePath );
+        Workbook wb = WorkbookFactory.create( testcasesInputStream );
 
-        Sheet master = wb.getSheet("master");
-        Row firstRowMaster = master.getRow(0);
-        Row secondRowMaster = master.getRow(1);
-        Row thirdRowMaster = master.getRow(2);
-        Row fourthRowMaster = master.getRow(3);
-        Row fifthRowMaster = master.getRow(4);
+        Sheet master = wb.getSheet( "master" );
+        Row firstRowMaster = master.getRow( 0 );
+        Row secondRowMaster = master.getRow( 1 );
+        Row thirdRowMaster = master.getRow( 2 );
+        // Row fourthRowMaster = master.getRow(3);
+        // Row fifthRowMaster = master.getRow(4);
 
-        int firstRowOfTestcase = (int) firstRowMaster.getCell(1).getNumericCellValue() - 1;
-        int lastRowOfTestcase = (int) secondRowMaster.getCell(1).getNumericCellValue() - 1;
-        int autoColumn = firstRowMaster.getCell(4).getStringCellValue().toCharArray()[0] - 'A';
-        int stepColumn = secondRowMaster.getCell(4).getStringCellValue().toCharArray()[0] - 'A';
-        int expectColumn = thirdRowMaster.getCell(4).getStringCellValue().toCharArray()[0] - 'A';
-        int resultColumn = fourthRowMaster.getCell(4).getStringCellValue().toCharArray()[0] - 'A';
-        int noteColumn = fifthRowMaster.getCell(4).getStringCellValue().toCharArray()[0] - 'A';
+        int firstRowOfTestcase = (int) firstRowMaster.getCell( 1 ).getNumericCellValue() - 1;
+        int lastRowOfTestcase = (int) secondRowMaster.getCell( 1 ).getNumericCellValue() - 1;
+        int autoColumn = firstRowMaster.getCell( 4 ).getStringCellValue().toCharArray()[0] - 'A';
+        int stepColumn = secondRowMaster.getCell( 4 ).getStringCellValue().toCharArray()[0] - 'A';
+        // int expectColumn = thirdRowMaster.getCell(4).getStringCellValue().toCharArray()[0] - 'A';
+        // int resultColumn = fourthRowMaster.getCell(4).getStringCellValue().toCharArray()[0] - 'A';
+        // int noteColumn = fifthRowMaster.getCell(4).getStringCellValue().toCharArray()[0] - 'A';
 
-        Sheet sheet = wb.getSheetAt(0);
-        for (int i = firstRowOfTestcase; i <= lastRowOfTestcase; i++) {
-            Row tcRow = sheet.getRow(i);
-            String tcId = tcRow.getCell(0).getStringCellValue();
-            String autoTest = tcRow.getCell(autoColumn).getStringCellValue();
-            String step = tcRow.getCell(stepColumn).getStringCellValue();
-            String expect = tcRow.getCell(expectColumn).getStringCellValue();
-            String result = tcRow.getCell(resultColumn).getStringCellValue();
-            String note = tcRow.getCell(noteColumn).getStringCellValue();
+        File file = new File( scriptFilePath );
+        FileWriter scriptFileWriter = new FileWriter( file );
+        scriptFileWriter.write( "begin" + NEW_LINE );
+
+        String updateTCs = thirdRowMaster.getCell( 1 ).getStringCellValue();
+
+        scriptFileWriter.write( TAB + "updateTCs: " + updateTCs );
+        // scriptFileWriter.write( TAB + "" +  + NEW_LINE);
+        scriptFileWriter.write( NEW_LINE );
+
+        Sheet sheet = wb.getSheetAt( 0 );
+        for ( int i = firstRowOfTestcase; i <= lastRowOfTestcase; i++ )
+        {
+            Row tcRow = sheet.getRow( i );
+
+            String testcaseId = tcRow.getCell( 0 ).getStringCellValue();
+            String testcaseAuto = tcRow.getCell( autoColumn ).getStringCellValue();
+            String testcaseStep = tcRow.getCell( stepColumn ).getStringCellValue();
+
+            scriptFileWriter.write( NEW_LINE +  TAB + "testcaseId: " + testcaseId );
+            scriptFileWriter.write( TAB + "testcaseAuto: " + testcaseAuto + NEW_LINE);
+            scriptFileWriter.write( TAB + "testcaseStep: " + testcaseStep + NEW_LINE);
             
-            System.out.print("\nid " + tcId);
-            System.out.print("\tauto:" + autoTest);
-            System.out.print("\n\tstep:" + step);
-            System.out.print("\texpect:" + expect);
-            System.out.print("\tresult:" + result);
-            System.out.print("\tnote:" + note);
+            System.out.print( "\nid " + testcaseId );
+            System.out.print( "\tauto:" + testcaseAuto );
+            System.out.print( "\n\tstep:" + testcaseStep );
         }
+
+        scriptFileWriter.write( "end" );
+        // Đóng luồng
+        scriptFileWriter.close();
     }
 }
