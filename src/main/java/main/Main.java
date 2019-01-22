eb5b8d9cb19ece767bb4fb8e8db70a517f7d6491
Thanh Diep Van <thanh.diep@ntq-solution.com.vn>
2019-01-22 18:21:42 +0700

    init command line parsers

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,25 +1,59 @@
 package main;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import selenium.SeleniumRunner;
 import spring.config.AppConfiguration;
 
-public class Main {
-	@Autowired
-	private SeleniumRunner scriptRunner;
+public class Main
+{
+    private static ApplicationContext context;
 
-	private static ApplicationContext context;
+    public static void main( String[] args )
+    {
+        context = new AnnotationConfigApplicationContext( AppConfiguration.class );
+        Main mainApp = (Main) context.getBean( "main" );
+        mainApp.run( args );
+        Options options = new Options();
 
-	public static void main(String[] args) {
-		context = new AnnotationConfigApplicationContext(AppConfiguration.class);
-		Main mainApp = (Main) context.getBean("main");
-		mainApp.run();
-	}
+        Option input = new Option("i", "input", true, "input file path");
+        input.setRequired(true);
+        options.addOption(input);
 
-	private void run() {
-		scriptRunner.access("");
-	}
+        Option output = new Option("o", "output", true, "output file");
+        output.setRequired(true);
+        options.addOption(output);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+
+            System.exit(1);
+        }
+
+        String inputFilePath = cmd.getOptionValue("input");
+        String outputFilePath = cmd.getOptionValue("output");
+
+        System.out.println(inputFilePath);
+        System.out.println(outputFilePath);
+    }
+
+    private void run(String[] args)
+    {
+        System.out.println( "xxxxxxxxxxxxxx" );
+    }
 }
