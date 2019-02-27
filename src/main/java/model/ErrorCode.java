dc5db657b6ef5afca8f2d1c822894c6078515d09
Thanh Diep Van <thanh.diep@ntq-solution.com.vn>
2019-02-27 18:51:43 +0700

    - add more error code

--- src/main/java/model/ErrorCode.java
+++ src/main/java/model/ErrorCode.java
@@ -2,5 +2,7 @@ package model;
 
 public class ErrorCode
 {
-    public static final int WRONG_BROWSER_NAME = 100;
+    public static final int WRONG_BROWSER_NAME = 99;
+    public static final int DRIVER_NOT_FOUND = 100;
+    public static final int BROWSER_AND_DRIVER_NOT_MATCH = 101;
 }
