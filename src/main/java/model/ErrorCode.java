f84566b9691774bd4f656264f78d6606c4b05b71
Thanh Diep Van <thanh.diep@ntq-solution.com.vn>
2019-03-01 18:30:29 +0700

    Add more ErrorCode

--- src/main/java/model/ErrorCode.java
+++ src/main/java/model/ErrorCode.java
@@ -5,4 +5,8 @@ public class ErrorCode
     public static final int WRONG_BROWSER_NAME = 99;
     public static final int DRIVER_NOT_FOUND = 100;
     public static final int BROWSER_AND_DRIVER_NOT_MATCH = 101;
+
+    public static final int TESTCASE_FILE_NOT_FOUND = 800;
+    public static final int TESTCASE_FILE_ENCRYPTED = 801;
+    public static final int IO_EXCEPTION_SCRIPT_GENERATOR = 850;
 }
