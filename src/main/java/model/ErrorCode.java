9d8a3d8838a4164fb5f8dbd93bcb4d11a1e71a75
Thanh Diep Van <thanh.diep@ntq-solution.com.vn>
2019-03-20 15:00:12 +0700

    Add more Error Code

--- src/main/java/model/ErrorCode.java
+++ src/main/java/model/ErrorCode.java
@@ -2,14 +2,23 @@ package model;
 
 public class ErrorCode
 {
+    /*
+     * Program: 0-9
+     * Browser and Driver: 1xx
+     * Test case file: 8xx
+     * UNKNOWN - 4xx
+     */
     public static final int PROGRAM_EXCUTE_SUCCESSED = 0;
     public static final int WRONG_PARAMETER = 1;
 
-    public static final int WRONG_BROWSER_NAME = 99;
-    public static final int DRIVER_NOT_FOUND = 100;
-    public static final int BROWSER_AND_DRIVER_NOT_MATCH = 101;
+    public static final int WRONG_BROWSER_NAME = 100;
+    public static final int DRIVER_NOT_FOUND = 101;
+    public static final int BROWSER_AND_DRIVER_NOT_MATCH = 102;
 
     public static final int TESTCASE_FILE_NOT_FOUND = 800;
     public static final int TESTCASE_FILE_ENCRYPTED = 801;
+    public static final int TESTCASE_FILE_IN_USE = 810;
     public static final int IO_EXCEPTION_SCRIPT_GENERATOR = 850;
+
+    public static final int UNKNOWN_SELENE_ERROR = 400;
 }
