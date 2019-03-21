9455ed3185ece66794a0a8ff845416e4f4b89859
Thanh Diep Van <thanh.diep@ntq-solution.com.vn>
2019-03-21 10:59:02 +0700

    Update example

--- src/main/java/jquery/InjectJQueryExample.java
+++ src/main/java/jquery/InjectJQueryExample.java
@@ -6,25 +6,24 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+/*
+ * For example only, when run that class --> Error!
+ */
 public class InjectJQueryExample
 {
-    // lenh height() trong JQuery
     private static String HEIGHT_JQ = "return $(document ).height();";
-    private static String JQUERY_FILE = "src\\test\\selenium\\injectjquery\\script.txt";
     private static String ENABLE_ELEMENT = "var value = $(\"[name='_eventId_modify']\");"
             + "value[0].disabled = false;";
     public static void main(WebDriver driver) throws IOException {
         driver.manage().window().maximize();
         WebElement editE = driver.findElement( By.id( "_edit" ) );
         editE.click();
-        // Inject JQuery trong Selenium WebDriver
-        JQueryUtils.injectJQuery(driver, JQUERY_FILE);
-        // tinh chieu cao cua trang viettuts.vn
+        JQueryUtils.injectJQuery(driver);
         JavascriptExecutor jse = ((JavascriptExecutor) driver);
         long pageHeight = (Long) jse.executeScript(HEIGHT_JQ);
         jse.executeScript( ENABLE_ELEMENT );
         System.out.println("$(document ).height() = " + pageHeight);
-        //driver.quit();
+        driver.quit();
     }
 
 }
